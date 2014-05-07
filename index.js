89155c5701bf96fa20ec353fc3de7550b1ec29ca
PowerQi <frfgzzq@aliyun.com>
2014-05-08 03:43:39 +0800

    lesson2

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
+var makeJade = require('./lib/processor/jade.js');
+var makeLess = require('./lib/processor/less.js');
+var renderRoot = require('./lib/processor/root.js');
+var doReject = require('./lib/processor/reject.js');
 
 module.exports = function(dir){
 
   var app = connect();
   app.use(function(request,response,next){
     var url = request.url;
-    console.log(url);
-    if(url == "/current-time"){
+    if(url == "/current-time") {
       response.end((new Date()).toISOString());
-    } else {
-      console.log('go next');
+    }
+    else {
       next();
     }
   });
-  app.use(serveStatic(dir, {'index': 'default.html'}));
+  app.use(doReject);
+  app.use(renderRoot); 
+  app.use(serveStatic(dir));
+  app.use(makeJade(dir));  
+  app.use(makeLess(dir));
 
+  
   return app;
 }
